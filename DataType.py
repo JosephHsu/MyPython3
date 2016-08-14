9517a39d03bb177532a966d605330a27e075ff6f
JosephHsu <JosephHsu@JosephHsudeMacBook-Air.local>
2016-08-15 00:18:23 +0800

    first commit

---
+++ DataType.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+數值型態 ：
+    整數 int ,Py3之後就不再有分int ,long
+    浮點數 float
+    布林 bool , True and False(None,0,0.0,0j,'' 空字串,() tuple,[] list,{} dict)
+    複數 complex , a+bj
+"""
+print("===============以下分別為10的十進位,二進位,八進位,十六進位")
+print(10)
+print(0b1010)
+print(0o12)
+print(0xA)
+
+print("===============驗證某個值得型態，可type()")
+print(type(10))
+print(type(0xA))
+
+print("===============轉換型態，可用int(),float(),bool()")
+print(int('10'))
+print(int(True))
+print(int(False))
+print(int(3.1415926))
+
+print(float('3.14152936'))
+
+print(bool(1))
+
+
+
+
+
+
+
